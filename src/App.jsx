40a77288a6b8fd2502d00e7af95e5c99cc5ce79c
rkr-dev <50330315+rkr-dev@users.noreply.github.com>
2022-08-08 18:33:32 -0700

    Initial Commit
     - Initial Commit
     - env template
     - Router Setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import Search from './pages/search';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/search' element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App
