5c6442dbf0a532d371cbf3e82c56681297e280b0
John Barrett <johnjbarrett22@gmail.com>
2022-08-18 08:00:03 -0700

    Most changes made from initial pull request

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -2,88 +2,83 @@ import React from 'react'
 
 const SearchForm = () => {
   return (
-    <div className="py-12 bg-white">
+    <div className="px-12 py-6 bg-white font-['Verdana']">
         <form>
             <div> {/* City Selection */}
                 <div>
-                    <label>Pick the cities to compare:</label>
+                    <p className="text-center text-xl text-[#067272]">Pick the cities to compare</p>
                 </div>
                 <div>
-                    <p>Minimum 2</p>
+                    <p className="text-center text-xs text-[#067272]">Minimum 2</p>
                 </div>
-                <div>
+                <div className="grid gap-3 grid-cols-2 grid-rows-3 mt-4">
                     <button
                         type="button"
-                        className="ml-5 bg-grey py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        className="bg-grey py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50">
                         San Diego, CA
                     </button>
                     <button
                         type="button"
-                        className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        className="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50">
                         Atlanta, GA
                     </button>
-                </div>
-                <div>
                     <button
                         type="button"
-                        className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        className="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50">
                         Dallas, TX
                     </button>
                     <button
                         type="button"
-                        className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        className="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50">
                         Nashville, TN
                     </button>
-                </div>
-                <div>
                     <button
                         type="button"
-                        className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        className="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50">
                         Phoenix, AZ
                     </button>
                     <button
                         type="button"
-                        className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        className="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50">
                         Jacksonville, FL
                     </button>
                 </div>
             </div>
 
-            {/* <div>
-                <label>Beds</label>
-                <div>
-                    <input type="radio">1 Bedroom</input>
-                    <label>Moo</label>
-                </div> 
-            </div> */}
+            <p className="text-center text-2xl mt-10 text-[#067272]">Apartment Features</p>
+
+            <div> {/* Bed Quantity */}
+                <p className="text-center text-xl mt-4 text-[#067272]">Beds</p>
+                <div className="flex justify-evenly mt-4">
+                    <input type="radio" value="1-bedroom" name="rooms" className="checked:bg-red-300"/>1 Bedroom
+                    <input type="radio" value="2-bedroom" name="rooms"/>2 Bedrooms
+                    <input type="radio" value="3-bedroom" name="rooms"/>3 Bedrooms
+                </div>
+            </div>
 
             <div> {/* Rent Range */}
                 <div> 
-                    <label>Monthly Rent</label>
+                    <p className="text-center text-xl mt-10 text-[#067272]">Monthly Rent</p>
                 </div>
-                <div className="grid grid-cols-6 gap-6">
-                    <div className="col-span-6 sm:col-span-3">
-                        <label htmlFor="low-end" className="block text-sm font-medium text-gray-700">
-                        Low End
-                        </label>
+                <div className="grid grid-cols-2 gap-6 mt-4">
+                    <div>
                         <input
                             type="text"
-                            name="low-end"
-                            id="low-end"
+                            name="min"
+                            id="min"
+                            placeholder="Min"
                             autoComplete="given-value"
-                            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                            className="border border-gray-300 py-2"
                         />
                     </div>
-                    <div className="col-span-6 sm:col-span-3">
-                        <label htmlFor="high-end" className="block text-sm font-medium text-gray-700">
-                        High End
-                        </label>
+                    <div>
                         <input
                             type="text"
-                            name="high-end"
-                            id="high-end"
+                            name="max"
+                            id="max"
+                            placeholder="Max"
                             autoComplete="given-value"
-                            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                            className="border border-gray-300 py-2"
                         />
                     </div>
                 </div>
@@ -91,57 +86,52 @@ const SearchForm = () => {
 
             <div> {/* Amenity Selection */}
                 <div>
-                    <label>Amenities</label>
+                    <p className="text-center text-xl mt-10 text-[#067272]">Amenities</p>
                 </div>
                 <div>
-                    <p>Minimum 2</p>
+                    <p className="text-center text-xs text-[#067272]">Minimum 2</p>
                 </div>
-                <div>
+                <div className="grid gap-3 grid-cols-2 grid-rows-3 mt-4">
                     <button
                         type="button"
-                        className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        className="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50">
                         Pet Friendly
                     </button>
                     <button
                         type="button"
-                        className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        className="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50">
                         Gated
                     </button>
-                </div>
-                <div>
                     <button
                         type="button"
-                        className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        className="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50">
                         In-Unit Washer/Dryer
                     </button>
                     <button
                         type="button"
-                        className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        className="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50">
                         Pool
                     </button>
-                </div>
-                <div>
                     <button
                         type="button"
-                        className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        className="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50">
                         Fitness Center
                     </button>
                     <button
                         type="button"
-                        className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        className="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50">
                         Parking
                     </button>
                 </div>
             </div>
 
-            <hr></hr>
-
-            <button     
-                type="button"
-                className="ml-5 bg-blue py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                Submit
-            </button>
-
+            <div className="grid grid-cols-1 grid-rows-1 mt-12">
+                <button     
+                    type="button"
+                    className="py-2 px-2 border border-black rounded-lg shadow-sm text-sm font-medium hover:bg-[#067272] hover:text-[#ffffff] bg-[#A1D2DE] text-[#067272]">
+                    Search
+                </button>
+            </div>
         </form>
     </div>
   )
