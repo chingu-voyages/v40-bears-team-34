db5ca5f6f1a47b3c7d515f712099a57f3d7c7142
rkr-dev <50330315+rkr-dev@users.noreply.github.com>
2022-09-04 10:49:35 -0700

    Apartment  - Apartment page

--- src/pages/Apartment.jsx
+++ src/pages/Apartment.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import SoloApartment from '../components/SoloApartment'
 
 const Apartment = () => {
+    const {id} = useParams()
     return (
         <div className="text-2xl min-h-[90vh] flex flex-col items-center justify-center bg-[#f2f2f2]">
             <SoloApartment />
