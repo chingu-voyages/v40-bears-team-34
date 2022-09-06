7db7d640912ccf0763bb309451addd9826dc79a0
rkr-dev <50330315+rkr-dev@users.noreply.github.com>
2022-09-06 05:48:07 -0700

    Search Reducer
     - Search Reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import apartmentReducer from '../features/apartment/apartmentSlice'
 import apartmentsReducer from '../features/apartment/apartmentsSlice'
+import searchResultsReducer from '../features/apartment/searchSlice'
+
 const store = configureStore({
     reducer: {
         apartments: apartmentsReducer,
         apartment: apartmentReducer,
+        searchResults: searchResultsReducer,
     },
 })
 
